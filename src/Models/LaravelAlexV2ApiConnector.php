f2edb9d2d477319511b4df2a74b2431f848f6e4a
Mike <mickael.jombart@davikingcode.com>
2020-04-16 17:47:45 +0200

    Debug AlexV2Api.

--- src/Models/LaravelAlexV2ApiConnector.php
+++ src/Models/LaravelAlexV2ApiConnector.php
@@ -43,40 +43,40 @@ class LaravelAlexV2ApiConnector
     // 6.1	Authentification d’un compte technique applicatif au Webservice ALEXV2 (service : ws_auth_cta)
     public function ws_auth_cta()
     {
-        $client = new Client();
-        $endpoint = $this->api_url . "ws_auth_cta";
-        $headers = ['Content-Type' => 'application/json'];
-        $content = ['av2_S_ct_identifiant' => $this->sp_entity_id, 'av2_S_ct_mdp' => $this->api_password];
-
-        $response = $client->post(
-            $endpoint, [
-                'json' => $content,
-                'headers' => $headers,
-//                'connect_timeout' => 650,
-                'cert' => $this->cert,
-                'ssl_key' => $this->ssl_key,
-            ]
-        );
-
-        return json_decode($response->getBody()->getContents())->jeton;
+//        $client = new Client();
+//        $endpoint = $this->api_url . "ws_auth_cta";
+//        $headers = ['Content-Type' => 'application/json'];
+//        $content = ['av2_S_ct_identifiant' => $this->sp_entity_id, 'av2_S_ct_mdp' => $this->api_password];
+//
+//        $response = $client->post(
+//            $endpoint, [
+//                'json' => $content,
+//                'headers' => $headers,
+////                'connect_timeout' => 650,
+//                'cert' => $this->cert,
+//                'ssl_key' => $this->ssl_key,
+//            ]
+//        );
+//
+//        return json_decode($response->getBody()->getContents())->jeton;
 
 //        // Autre méthode : Curl (fonctionne).
-//        $ch = curl_init();
-//        curl_setopt($ch, CURLOPT_URL, 'https://alexv2-api-recn1.enedis.fr:10443/alex-api/av2/ws_auth_cta');
-//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-//        curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"av2_S_ct_identifiant\": \"SP-OC0-recn1\",\"av2_S_ct_mdp\": \"SKLtJMh-cU\"}");
-//        curl_setopt($ch, CURLOPT_POST, 1);
-//        curl_setopt($ch, CURLOPT_SSLCERT, $this->cert);
-//        curl_setopt($ch, CURLOPT_SSLKEY, $this->ssl_key);
-//        $headers = array();
-//        $headers[] = 'Content-Type: application/json';
-//        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-//        $result = curl_exec($ch);
-//        if (curl_errno($ch)) {
-//            echo 'Error:' . curl_error($ch);
-//        }
-//        curl_close($ch);
-//        return json_decode($result)->jeton;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->api_url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, '{\"av2_S_ct_identifiant\": \"'.$this->sp_entity_id.'\",\"av2_S_ct_mdp\": \"'.$this->api_password.'\"}');
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_SSLCERT, $this->cert);
+        curl_setopt($ch, CURLOPT_SSLKEY, $this->ssl_key);
+        $headers = array();
+        $headers[] = 'Content-Type: application/json';
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        $result = curl_exec($ch);
+        if (curl_errno($ch)) {
+            echo 'Error:' . curl_error($ch);
+        }
+        curl_close($ch);
+        return json_decode($result)->jeton;
     }
 
 }
